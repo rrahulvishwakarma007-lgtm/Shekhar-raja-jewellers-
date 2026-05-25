594f4cea3c99c23eefedc7412fe916239f76b10c
rrahulvishwakarma007-lgtm <rrahulvishwakarma007@gmail.com>
2026-05-25 12:58:11 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -50,18 +50,16 @@ const heroSlides = [
 
 // Categories data
 const categories = [
-  { name: 'Bridal', image: '/bridal.png' },
-  { name: 'Festive', image: '/festive.png' },
+  { name: 'Bridal', image: '/necklace1.png' },
+  { name: 'Festive', image: '/necklace2.png' },
   { name: 'Dailywear', image: 'https://images.pexels.com/photos/1191531/pexels-photo-1191531.jpeg?auto=compress&cs=tinysrgb&w=400' },
   { name: 'Necklaces', image: '/bridal.png' },
   { name: 'Earrings', image: '/antique.png' },
-  { name: 'Bangles', image: '/band.png' },
-  { name: 'Rings', image: '/band.png' },
-  { name: 'Pendants', image: 'https://images.pexels.com/photos/1191531/pexels-photo-1191531.jpeg?auto=compress&cs=tinysrgb&w=400' },
-  { name: 'Diamond', image: '/band.png' },
-  { name: 'Chains', image: '/band.png' },
+  { name: 'Bangles', image: '/bangle1.png' },
+  { name: 'Rings', image: '/ring1.png' },
+  { name: 'Diamond', image: '/ring2.png' },
   { name: 'Antique', image: '/antique.png' },
-  { name: 'Temple', image: '/temple.png' }
+  { name: 'Elegant', image: '/temple.png' }
 ];
 
 // Collections data
